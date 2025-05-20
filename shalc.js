b8f01aa3251bdeadd8c8e1c2682e3aa517ee8e15
niklas <niklas@appli.se>
2025-05-20 11:52:38 +0200

    Update the example, and add rust code generation

--- shalc.js
+++ shalc.js
@@ -1,21 +1,18 @@
-// test.js
-const fs = require('fs');
 const { parse, ParseError } = require('./hal_parser');
-const { astToIR } = require('./hal_ir');
+const { genRust } = require('./hal_codegen_rust');
 
-const halCode = `
-function integer foo(integer x, integer y)
-begin
-    z = x + y;
-    return z;
-end;
-`;
-
+let ast;
 try {
-    const ast = parse(halCode);
-    console.dir(ast, { depth: 10 });
-    const ir = astToIR(ast);
-    console.dir(ir, { depth: 10 });
+    const code = `
+        global
+        function integer foo(integer x, integer y)
+        begin
+            integer z;
+            z = x + y;
+            return z;
+        end;
+    `;
+    ast = parse(code);
 } catch (e) {
     if (e instanceof ParseError) {
         console.error(e.message);
@@ -24,3 +21,7 @@ try {
         throw e;
     }
 }
+console.dir(ast, { depth: 10 });
+
+console.log("\n// ---- Rust Output ----\n");
+console.log(genRust(ast));
